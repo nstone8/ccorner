7d1e709cb7241c307c549ea6c7329280b4e588bc
Stone <nstone8@gatech.edu>
2019-04-23 14:43:04 -0400

    added Date, bean and roast to recorded metrics

--- ccorner.py
+++ ccorner.py
@@ -1,8 +1,12 @@
 import sys
 import os
+import datetime
 import pandas as pd
 thisDir=sys.path[0]
 saveFile=os.path.join(thisDir,'ratings.csv')
+date=datetime.datetime.today().strftime('%m-%d-%Y')
+bean=input('Bean:')
+roast=input('Roast:')
 stir=input('Stir:')
 water=input('Water:')
 mass=input('Coffee Mass:')
@@ -10,7 +14,10 @@ body=input('Body:')
 bitter=input('Bitterness:')
 acid=input('Acidity:')
 flavor=input('Flavor:')
-thisRating=pd.DataFrame(dict(stir=[stir],
+thisRating=pd.DataFrame(dict(date=[date],
+	bean=[bean],
+	roast=[roast],
+	stir=[stir],
     water=[water],
     mass=[mass],
     body=[body],
